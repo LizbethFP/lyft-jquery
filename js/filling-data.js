4093a2921465611b0edaf1265bad0ff919b8d148
LizbethFP <lizbeth.fp@gmail.com>
2017-12-28 19:55:47 -0500

    corrigiendo validación de campos en vista de llenado de datos

--- js/filling-data.js
+++ js/filling-data.js
@@ -7,45 +7,63 @@ function begin() {
 
 // Crear función para validar que se ha ingresado los datos necesario: el nombre, apellido y correo electrónico
 function validatePersonalData() {
-  // validateFirstName();
-  // validateLastName();
-  // validateEmail();
+  validateFirstName();
+  validateLastName();
+  validateEmail();
 }
 
-// function validateFirstName() {
-//   // crear variable para seleccionar el input correspondiente al dato que se pide
-//   var firstName = $('input').first();
-//   // Agregar evento para capturar el valor del nombre
-//   $(firstName).on('input', function(event) {
-//     // console.log(event.target.value);
-//     console.log($(this).val());;
-//   });
-// }
+function validateFirstName() {
+  // crear variable para seleccionar el input correspondiente al dato que se pide
+  var firstName = $('input').first();
+  // Agregar evento para capturar el valor del nombre
+  $(firstName).on('input', function(event) {
+    // console.log(event.target.value);
+    // console.log($(this).val());;
+    if ($(this).val() !== '') {
+      enableButton();
+    } else if ($(this).val() === '') {
+      disableButton();
+    }
+  });
+}
 
-// function validateLastName() {
-//   // crear variable para seleccionar el input correspondiente al dato que se pide
-//   var lastName = $('input').first().next();
-//   // Agregar evento para capturar el valor del apellido
-//   $(lastName).on('input', function(event) {
-//     console.log($(this).val());;
-//   });
-// }
+function validateLastName() {
+  var validateLastName = false;
+  // crear variable para seleccionar el input correspondiente al dato que se pide
+  var lastName = $('input').first().next();
+  // Agregar evento para capturar el valor del apellido
+  $(lastName).on('input', function(event) {
+    // console.log($(this).val());;
+    if ($(this).val() !== '') {
+      validateLastName = true;
+      enableButton();
+    } else if ($(this).val() === '') {
+      disableButton();
+    }
+  });
+  return validateLastName;
+}
 
-// function validateEmail() {
-//   // crear variable para seleccionar el input correspondiente al dato que se pide
-//   var email = $('input').first().next().next();
-//   var REGULAREXPRESSION = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-//   // Agregar evento para capturar el valor del email
-//   $(email).on('input', function(event) {
-//     // console.log($(this).val());
-//     // console.log(REGULAREXPRESSION.test($(this).val()));
-//     if (REGULAREXPRESSION.test($(this).val())) {
-//       alert('es válido');
-//     } else {
-//       console.log('no es válido');
-//     }
-//   });
-// }
+function validateEmail() {
+  var validateEmail = false;
+  // crear variable para seleccionar el input correspondiente al dato que se pide
+  var email = $('input').first().next().next();
+  var REGULAREXPRESSION = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+  // Agregar evento para capturar el valor del email
+  $(email).on('input', function(event) {
+    // console.log($(this).val());
+    // console.log(REGULAREXPRESSION.test($(this).val()));
+    if (REGULAREXPRESSION.test($(this).val())) {
+      // alert('es válido');
+      validateEmail = true;
+      enableButton();
+    } else {
+      // console.log('no es válido');
+      disableButton();
+    }
+  });
+  return validateEmail;
+}
 
 function checkedBox() {
   // crear variable para seleccionar el input correspondiente al checkbox
@@ -65,13 +83,13 @@ function checkedBox() {
   });
 }
 
-
+/* FUNCIÓN PARA HABILITAR BOTON */
 function enableButton() {
   // Crear variable para seleccionar el botón que permitirá pasar a la siguiente vista
   var nextButton = $('.next-btn');
   console.log(nextButton);
   // Crear condicional que valide los anteriores campos
- // if (something === true) {
+  if (validateEmail && validateLastName && validateFirstName && checkedBox) {
     $(nextButton).attr('disabled', false);
     // Agregar color blanco a la fuente y rosado al fondo en el botón después de validar y hacer que tenga negrita
     $(nextButton).css({
@@ -80,9 +98,10 @@ function enableButton() {
       'color': '#fff',
       'fontWeight': 'bold'
     });
-  // };
+  };
 }
 
+/* FUNCIÓN PARA DESHABILITAR BOTON */
 function disableButton() {
   var nextButton = $('.next-btn');
   $(nextButton).attr('disabled', true);
