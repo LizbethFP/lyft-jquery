1b54bc1998a3f59bbd2985c3d001f7938239bcd7
LizbethFP <lizbeth.fp@gmail.com>
2017-12-30 00:33:57 -0500

    corrigiendo estilos en CSS y agregando vista splash

--- js/app.js
+++ js/app.js
@@ -1,19 +1,24 @@
 function begin() {
   transitionPage();
-  stopTransitionPage();
+  // stopTransitionPage();
 };
 
 
 // Aplicar transición de página index a vista principal
 function transitionPage() {
+  // Crear variable para el elemento a animar
+  var imgTransition = $('img');
+  // Crear efecto de toggle al elemento
+  $(imgTransition).slideToggle(3000);
+  // Redireccionar a siguiente página en 6 segundos
   setTimeout(function() {
     window.location.href = 'views/main-view.html';
   }, 5000);
 };
 
 // Detener transición de página
-function stopTransitionPage() {
-  clearTimeout(transitionPage());
-};
+// function stopTransitionPage() {
+//   clearTimeout(transitionPage());
+// };
 
 $(document).ready(begin);
